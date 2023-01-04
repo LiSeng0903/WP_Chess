dc59d7a29d286d9f8905faabfb0d175b3325db96
PuSung <sam90.hsu@gmail.com>
2023-01-04 15:43:24 +0800

    mongo connection

---
+++ backend/src/mongo.js
@@ -0,0 +1,24 @@
+// import 
+import mongoose from 'mongoose'
+import dotenv from 'dotenv-defaults'
+
+const mongo = {
+    connect: () => {
+        dotenv.config()
+        if ( !process.env.MONGO_URL ) {
+            console.error( 'Missing MONGO_URL' )
+            process.exit( 1 )
+        }
+
+        mongoose.connect( process.env.MONGO_URL, {
+            useNewUrlParser: true,
+            useUnifiedTopology: true
+        } ).then( ( res ) => {
+            console.log( 'mongo db connection created' )
+        } )
+
+        mongoose.connection.on( 'error', console.error.bind( console, 'connection error:' ) )
+    }
+}
+
+export default mongo
--- backend/src/server.js
+++ backend/src/server.js
@@ -4,34 +4,44 @@ import http from 'http'
 import WebSocket from 'ws'
 import wsConnect from "./wsConnect.js"
 import { uuid } from 'uuidv4'
+import mongoose from "mongoose"
+import mongo from './mongo.js'
 
 // Constant 
 const SERVER_IP = 'localhost'
+
+//mongoose connection
+mongoose.set( 'strictQuery', false )
+mongo.connect()
 
 // Server 
 const app = express()
 const server = http.createServer( app )
 const serverWS = new WebSocket.Server( { server } )
+const db = mongoose.connection
 
 // Store infomations 
 let games = [] // list of Game object 
 let connections = [] // list of connection info, index is connection ID; { ws:.., name:.., game:.. }
 
-serverWS.on( "connection", ( clientWS ) => {
+db.once( 'open', () => {
+    console.log( 'db connected' )
 
-    // store connection 
-    let connectionID = uuid()
-    connections[connectionID] = {
-        ws: clientWS,
-        name: '',
-        game: ''
-    }
+    serverWS.on( "connection", ( clientWS ) => {
+        // store connection 
+        let connectionID = uuid()
+        connections[connectionID] = {
+            ws: clientWS,
+            name: '',
+            game: ''
+        }
 
-    clientWS.onmessage = wsConnect.onMessage( clientWS, connectionID, games, connections )
-    console.log( 'player connect' )
+        clientWS.onmessage = wsConnect.onMessage( clientWS, connectionID, games, connections )
+        console.log( 'player connect' )
+    } )
 } )
 
-const PORT = 4000
+const PORT = process.env.PORT || 4000
 server.listen( PORT, SERVER_IP, () => {
     console.log( `server is on ${PORT}` )
 } )
